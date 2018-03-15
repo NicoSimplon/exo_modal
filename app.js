933be1571aaa6cad63004b9635180d3206695c2c
NicoSimplon <marty.nicolas11@laposte.net>
2018-03-15 19:03:44 +0100

    commit initial

---
+++ app.js
@@ -0,0 +1,11 @@
+$("#reserv1").click(function(){
+	$("#exampleModalLabel").text($("#hotel1").text());
+})
+
+$("#reserv2").click(function(){
+	$("#exampleModalLabel").text($("#hotel2").text());
+})
+
+$("#reserv3").click(function(){
+	$("#exampleModalLabel").text($("#hotel3").text());
+})
